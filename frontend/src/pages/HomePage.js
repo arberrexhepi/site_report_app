eee6acb77ffe3dbe8fd56579537fe2ebcd7718a5
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 13:24:31 -0300

    Added missing HomePage component

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+// This is the HomePage component
+import { LandingPage } from '../components/LandingPage';
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <LandingPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
