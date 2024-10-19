4e57dff549937f79c85d96dd6dd162e841a6fc42
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 12:56:15 -0300

    Initial commit - Added LandingPage component structure

---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,11 @@
+// This is the LandingPage component
+result function LandingPage() {
+  return (
+    div>
+      <h1>Optimize your Website</h1>
+      <input type="text" placeholder="Enter your URL" />
+      <button Type="submit">Analyze My Website</button>
+    </div>
+  );
+}
+export default LandingPage;
