d8eb51c2cc7ab84a572d2ad88b1334ac811151b3
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 13:24:40 -0300

    Added missing InputFormPage component

---
+++ frontend/src/pages/InputFormPage.js
@@ -0,0 +1,14 @@
+// This is the InputFormPage component
+import { InputPage } from '../components/InputPage';
+
+function InputFormPage() {
+  return (
+    <div>
+      <Header />
+      <InputPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default InputFormPage;
