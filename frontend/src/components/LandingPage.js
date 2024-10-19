a3ea53b944ebbf86c655908bec7f3c9d6dd01341
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 13:20:13 -0300

    Added missing LandingPage component

---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,10 @@
+// This is the LandingPage component
+export function LandingPage() {
+  return (
+    <div>
+      <h1>Optimize your Website</h1>
+      <input type='text' placeholder='Enter URL' />
+      <button>Analyze My Website</button>
+    </div>
+  );
+}
