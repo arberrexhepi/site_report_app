437ddb61aec8f7eb2ee0e553ca88bb1ec5ef2980
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 13:44:20 -0300

    Added analyzeRoutes.test.js to treeblock branch

---
+++ backend/tests/analyzeRoutes.test.js
@@ -0,0 +1,10 @@
+// Test cases for analyzeRoutes api endpoint
+import supertest from 'supertest';
+
+const app = require('../app'); // Import your express server here
+describe('ANELIZE ROUTES', () => {
+  it('should tast return success');
+
+  it('should handle responses for valid inputs');
+
+});
