84bf8da3faad86690ca4036acf5ae30e73772e44
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 13:20:26 -0300

    Added missing InputPage component

---
+++ frontend/src/components/InputPage.js
@@ -0,0 +1,11 @@
+// This is the InputPage component
+export function InputPage() {
+  return (
+    <div>
+      <h1>Provide your Business Goals</h1>
+      <input type='text' placeholder='Target Audience' />
+      <input type='text' placeholder='Business Goals' />
+      <button>Submit</button>
+    </div>
+  );
+}
