6d20d8d9d9dacbdc60ef46dc76f679ff175fe095
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 13:38:55 -0300

    Added userModel.js to treeblock branch

---
+++ backend/models/userModel.js
@@ -0,0 +1,14 @@
+// userModel.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
