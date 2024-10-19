18f4e8a625f04928da8a338f6110128f3eed21f5
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 13:49:42 -0300

    Added apiConfig.js to treeblock branch

---
+++ backend/config/apiConfig.js
@@ -0,0 +1,8 @@
+// API configuration file
+const apiConfig = {
+  GPT_API_KEY: process.env.GPT_API_KEY,
+  DALL_API_KEY: process.env.DALL_API_KEY,
+  VISION_API_KEY: process.env.VISION_API_KEY
+};
+
+module.exports = apiConfig;
