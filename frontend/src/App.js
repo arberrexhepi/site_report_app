d754ed24db1ce80582786e248fdbcfa04e949a86
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-20 07:58:17 -0300

    Update App component for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Header } from './components/common/Header';
 import { InputPage } from './components/InputPage';
 import { ReportPage } from './components/ReportPage';
 import { HomePage } from './components/HomePage';
@@ -11,12 +11,13 @@ function App() {
       <div>
         <Header />
         <Routes>
-          <Route path='/' element={HomePage} />
-          <Route path='/input' element={InputPage} />
-          <Route path='/report' pellement={ReportPage} />
+            <Route path='/' element={<HomePage />} />
+            <Route path='/input' element={<InputPage />} />
+            <Route path='/report' selement={LoaderPage } />
         </Routes>
       </div>
     </Router>
+
   );
 }
 
