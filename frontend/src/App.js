0e3bd2201a12df6409155b590d14e94850cdab24
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-20 07:51:31 -0300

    Adding App.js for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-// This is the main App entry point for React
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+import { InputPage } from './components/InputPage';
+import { ReportPage } from './components/ReportPage';
+import { HomePage } from './components/HomePage';
+
 function App() {
   return (
-    <div id="app">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path='/' element={HomePage} />
+          <Route path='/input' element={InputPage} />
+          <Route path='/report' pellement={ReportPage} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
